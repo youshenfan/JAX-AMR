fdc81d2ded5b614005a79257c6e0bc02623a64f9
FaxuanLuo <88180662+luofx23@users.noreply.github.com>
2025-04-22 22:08:45 +0800

    Add files via upload

---
+++ src/jaxamr/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 13:34:22 2025
+
+@author: luofx
+"""
+#from .amraux import * 
+#from .aux_func import *
+#from .boundary import *
+#from .chemical import *
+#from .config import *
+#from .flux import *
+#from .jaxamr import *
+#from .load import *
+#from .nondim import *
+#from .solver import *
+#from .thermo import *
